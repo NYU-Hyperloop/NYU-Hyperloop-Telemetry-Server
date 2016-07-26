e3a21f90a79047d8f007aeaaec7735c39d841bf0
Eric Oswald <eoswald39@gmail.com>
2016-07-26 18:58:15 -0400

    Add comments to arduino test.

--- arduinotest/src/sketch.ino
+++ arduinotest/src/sketch.ino
@@ -1,9 +1,13 @@
 
+// Size of buffer of all sensor data and their timestamps
 const size_t DATA_BUFFER_SIZE = 6;
 
+// sensorData object and the data buffer share the same
+// location in memory. Makes it easy to collect data 
+// and ship it quickly.
 union Data {
     struct DataStruct {
-        char beginPad[2];
+        char beginPad[2]; // padding to easily find beginning
         char data1;
         char pad1;
         char data2;
@@ -12,8 +16,10 @@ union Data {
     char buffer[DATA_BUFFER_SIZE];
 } myData;
 
+// run once at startup
 void setup()
 {
+    // set dummy values
     myData.sensorData.beginPad[0] = 0x41;
     myData.sensorData.beginPad[1] = 0x42;
     myData.sensorData.data1 = 0x43;
@@ -21,12 +27,16 @@ void setup()
     myData.sensorData.data2 = 0x45;
     myData.sensorData.pad2 = 0x46;
 
+    // start serial
     Serial.begin(9600);
 }
 
+// runs continuously
 void loop()
 {
+    // send the data buffer
     Serial.write(myData.buffer, DATA_BUFFER_SIZE);
 
+    // delay to give it time to send
     delay(100);
 }
