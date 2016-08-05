9ba3ad20e87ef49f3d060fbc79481466f2bfb598
alexandru-grigoras <al.grigoras@yahoo.com>
2016-08-05 14:23:28 +0300

    Started implementing the GUI
    Made minor changes to the server to better handle clients and threads
    Added a new library for Gauges
    Added extra fields to FakeDataStruct
    Implemented a basic display of three pieces of data

--- fakeserial.py
+++ fakeserial.py
@@ -1,9 +1,20 @@
-from ctypes import Structure, c_int
+from ctypes import Structure, c_int, c_float
 import random
 import time
 
 class FakeDataStruct(Structure):
-    _fields_ = [('data1', c_int)]
+    _fields_ = [('yaw', c_float),
+                    ('pitch', c_float),
+                    ('roll', c_float),
+                    ('acceleration', c_float),
+                    ('velocity', c_float),
+                    ('rpm', c_int), 
+                    ('position', c_int),
+                    ('temperature_inside', c_float), 
+                    ('temperature_outside', c_float), 
+                    ('temperature_electronics', c_float),
+                    ('eta', c_float),   
+                    ]
 
 # Very raw implementation of a fake serial
 class Serial:
@@ -17,7 +28,18 @@ class Serial:
 
     def read(self):
         while True:
-            self.data_struct.data1 = random.randint(0,65535)
+            self.data_struct.yaw = random.randint(0,400)
+            self.data_struct.pitch = random.randint(0,400)
+            self.data_struct.roll = random.randint(0,400)
+            self.data_struct.acceleration = random.randint(0,400)
+            self.data_struct.velocity = random.randint(0,400)
+            self.data_struct.rpm = random.randint(0,400)
+            self.data_struct.position = random.randint(0,400)
+            self.data_struct.temperature_inside = random.randint(0,400)
+            self.data_struct.temperature_outside = random.randint(0,400)
+            self.data_struct.temperature_electronics = random.randint(0,400)
+            self.data_struct.eta = random.randint(0,400)
+
             self.serial_queue.put(self.data_struct)
             time.sleep(.1)
 
